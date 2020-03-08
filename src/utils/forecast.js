428f5911a545bac89c0ca62b8443eb6bb10337ff
Carlo Jude Alfonso <carlojude.alfonso@gmail.com>
2020-03-08 13:54:49 +0800

    Added additional forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -28,7 +28,11 @@ const forecast = (latitude, longitude, callback) => {
           undefined,
           body.daily.data[0].summary +
             " It is currently " +
-            body.currently.temperature
+            body.currently.temperature +
+            ". Today is " +
+            body.daily.data[0].temperatureHigh +
+            " with a low of " +
+            body.daily.data[0].temperatureLow
         );
       }
     }
