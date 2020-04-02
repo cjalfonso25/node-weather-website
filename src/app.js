3182e84c8b790d058501495915ebc4b8bdc864c6
Carlo Jude Alfonso <carlojude.alfonso@gmail.com>
2020-04-02 15:52:00 +0800

    Added CORS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,10 @@ app.use(express.static(publicDirectoryPath));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
